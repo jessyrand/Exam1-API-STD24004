729fb5e2f2b3deb278333eaef4c0ceef25bc0136
Jessy Randriamizara <henintsoarandriam@gmail.com>
2025-07-24 10:37:59 +0300

    added get welcome request

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from starlette.responses import Response
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
@@ -8,3 +8,8 @@ def root():
     with open("hello.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return Response(content=html_content, status_code=200, media_type="text/html")
+
+@app.get("/welcome")
+def welcome(name:str):
+    message = f"Welcome {name}"
+    return JSONResponse(content=message, status_code=200)
