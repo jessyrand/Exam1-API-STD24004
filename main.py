a422e5ef186a01f0c159eb1dade6784606269bd7
Jessy Randriamizara <henintsoarandriam@gmail.com>
2025-07-24 10:32:54 +0300

    added the hello request

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from starlette.responses import Response
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    with open("hello.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return Response(content=html_content, status_code=200, media_type="text/html")
